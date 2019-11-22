9ba08031b3e4e7e1f9d0032bdd59160c74781ffc
Josh Palmer <jpalmerwatkins@gmail.com>
2019-11-22 00:06:28 +0000

    Remove unused references from capsule_rotation

--- Src/Player/capsule_rotation.gd
+++ Src/Player/capsule_rotation.gd
@@ -2,8 +2,6 @@ extends Node
 
 var nodes = Util.NodeDependencies.new([
 	PN.Spatial.KINEMATIC_BODY,
-	PN.Spatial.COLLISION_SHAPE,
-	PN.Spatial.CAMERA_OFFSET
 ])
 
 export (Vector3) var rot_forward = Vector3.FORWARD
@@ -14,8 +12,6 @@ func _ready():
 
 func _physics_process(delta):
 	var player = nodes.get(PN.Spatial.KINEMATIC_BODY)
-	var shape = nodes.get(PN.Spatial.COLLISION_SHAPE)
-	var camera_offset = nodes.get(PN.Spatial.CAMERA_OFFSET)
 	
 	var dir = rot_forward.normalized()
 	var axis = dir.cross(Vector3.DOWN).normalized()
